e0c01ee0100574d9f136e7ff13d19d41f1e6989d
williamsimms <williamsimms193@gmail.com>
2021-02-03 07:56:00 -0500

    added definitions for methods

--- main.py
+++ main.py
@@ -22,20 +22,22 @@ class LinkedList:
         current = self.head
 
         while current:
-            linkedlist += int(current.data)
-            linkedlist += ' => '
+            linkedlist += str(current.data)
+            linkedlist += ' -> '
             current = current.next
 
         linkedlist += 'None'
 
+        return linkedlist
+
     def __repr__(self):
         return str(self.head)
 
-    def print(self):
+    def print_list(self):
         return str(self.head)
 
     def insert_first(self, data):
-        if not self.head:
+        if self.head is None and self.tail is None:
             node = Node(data)
             self.head = node
             self.tail = node
@@ -43,11 +45,53 @@ class LinkedList:
 
         node = Node(data, next=self.head)
         self.head = node
+        self.head.next.prev = self.head
 
     def insert_last(self, data):
+        pass
+
+    def insert_at(self, index: int, data):
+        pass
+
+    def remove_first(self):
+        pass
+
+    def remove_last(self, data):
+        pass
+
+    def remove_at(self, index: int):
+        pass
+
+    def reverse(self):
+        pass
+
+    def get_first(self):
+        pass
+
+    def get_at(self):
+        pass
+
+    def get_last(self):
+        pass
+
+    def __getitem__(self, index: int):
+        pass
+
+    def length(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def clear(self):
+        pass
+
+    def for_each(self):
         pass
 
 
 if __name__ == '__main__':
     linkedlist = LinkedList()
     linkedlist.insert_first(2)
+    linkedlist.insert_first(6)
+    linkedlist.insert_first(11)
